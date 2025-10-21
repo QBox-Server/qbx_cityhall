2b32bd18f737696c4eaf7cea0cb4e962a40d92d3
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2025-10-20 20:28:54 -0400

    Enable useTarget and update ped coordinates

--- config/client.lua
+++ config/client.lua
@@ -1,11 +1,11 @@
 return {
-    useTarget = false,
+    useTarget = true,
     debugPoly = false,
 
     peds = {
         { -- Cityhall Ped
             model = 'a_m_m_hasjew_01',
-            coords = vec4(-262.79, -964.18, 30.22, 181.71),
+            coords = vec4(230.18, -423.02, 47.92, 343.25),
             scenario = 'WORLD_HUMAN_STAND_MOBILE',
             cityhall = true,
             zoneOptions = { -- Used for when UseTarget is false
