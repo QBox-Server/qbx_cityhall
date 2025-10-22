d16a386a3049ec7667969deeb7090f45d32b19d7
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2025-10-21 20:57:52 -0400

    Update Cityhall Ped coordinates and rotation

--- config/client.lua
+++ config/client.lua
@@ -5,7 +5,7 @@ return {
     peds = {
         { -- Cityhall Ped
             model = 'a_m_m_hasjew_01',
-            coords = vec4(230.18, -423.02, 47.92, 343.25),
+            coords = vec4(230.24, -423.12, 46.92, 344.78),
             scenario = 'WORLD_HUMAN_STAND_MOBILE',
             cityhall = true,
             zoneOptions = { -- Used for when UseTarget is false
